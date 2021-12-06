cd760c0651e03ffde080ec87672d9843982759ec
hannachronholm <95073495+hannachronholm@users.noreply.github.com>
2021-12-06 22:19:06 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -1,15 +1,34 @@
 // When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function () {
-  scrollFunction();
-};
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 600 ||
-    document.documentElement.scrollTop > 600
-  ) {
-    document.getElementById("desktop-menu").style.top = "0";
-  } else {
-    document.getElementById("desktop-menu").style.top = "-54px";
-  }
-}
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 600 ||
+//     document.documentElement.scrollTop > 600
+//   ) {
+//     document.getElementById("desktop-menu").style.top = "0";
+//   } else {
+//     document.getElementById("desktop-menu").style.top = "-54px";
+//   }
+// }
+
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// hamburger.addEventListener("click", mobileMenu);
+
+// function mobileMenu() {
+//   hamburger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// }
+
+// const navLink = document.querySelectorAll(".nav-link");
+
+// navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+// function closeMenu() {
+//   hamburger.classList.remove("active");
+//   navMenu.classList.remove("active");
+// }
